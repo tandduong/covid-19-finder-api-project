c1f66933f932894eb9e873f99f97990ec6707be7
tandduong <tanduong1912@gmail.com>
2020-04-25 16:39:32 -0400

    Updates

---
+++ precache-manifest.6d2485c76584d780a13c06e23158faac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0708bc596df8c1959f9f43d41b832e99",
+    "url": "/covid-19-finder-api-project/index.html"
+  },
+  {
+    "revision": "dcfe864b36fe329ba9d9",
+    "url": "/covid-19-finder-api-project/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "bd204f102e16da70e27f",
+    "url": "/covid-19-finder-api-project/static/css/main.e774c4c8.chunk.css"
+  },
+  {
+    "revision": "dcfe864b36fe329ba9d9",
+    "url": "/covid-19-finder-api-project/static/js/2.682fc8e2.chunk.js"
+  },
+  {
+    "revision": "6d94984a5fdbad217098935a3d45084e",
+    "url": "/covid-19-finder-api-project/static/js/2.682fc8e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd204f102e16da70e27f",
+    "url": "/covid-19-finder-api-project/static/js/main.d9b037a0.chunk.js"
+  },
+  {
+    "revision": "df7eea8477f2d483bff9",
+    "url": "/covid-19-finder-api-project/static/js/runtime-main.6b745c62.js"
+  }
+]);
